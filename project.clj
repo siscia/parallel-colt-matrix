e53bcbc29bc40386efbaed0dc449282a1ccf3777
siscia <simone@mweb.biz>
2013-01-22 22:40:31 +0100

    add project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject parallel-colt-matrix "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [mikera/matrix-api "0.0.4"]
+                 [net.sourceforge.parallelcolt/parallelcolt "0.10.0"]
+                 [org.clojure/tools.nrepl "0.2.0"]])
