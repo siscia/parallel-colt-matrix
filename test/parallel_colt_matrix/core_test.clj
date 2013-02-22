843a970ede73674b5f60455ff2bdd79c789543e6
siscia <simone@mweb.biz>
2013-02-22 21:26:46 +0100

    add some test

--- test/parallel_colt_matrix/core_test.clj
+++ test/parallel_colt_matrix/core_test.clj
@@ -1,21 +1,32 @@
 (ns parallel-colt-matrix.core-test
-  (:use clojure.test)
+  (:use [clojure.test]
+        [clojure.core.matrix.protocols]
+        [clojure.core.matrix.compliance-tester])
   (:require [parallel-colt-matrix.core :as pc])
   (:import [cern.colt.matrix.tdouble.impl DenseDoubleMatrix2D]))
 
 (def mpc (DenseDoubleMatrix2D. 2 2))
-             
+(def m (construct-matrix mpc [[1 2 3] [4 5 6]]))
+
 (deftest vector-dimensionality-test
   (is (== 2 (pc/vector-dimensionality [[1 2 3]])))
   (is (== 3 (pc/vector-dimensionality [[[1] [2] [3] [4]]]))))
 
 (deftest implementation-key-test
-  (is (= :parallel-colt (pc/implementation-key mpc))))
+  (is (= :parallel-colt (implementation-key mpc))))
 
-;; (deftest construct-test
-;;   (is (= (construct-matrix mpc [[1 2 3] [3 4 5]]) [[1 2 3] [3 4 5]])))
+(deftest construct-test
+  (is (matrix-equals (construct-matrix mpc [[1 2 3] [3 4 5]]) (construct-matrix mpc  [[1 2 3] [3 4 5]]))))
 
-;; (deftest test-2-2-sparse
-;;   (let [m1 (DenseDoubleMatrix2D. 2 2)]
-;;     (println m1 (class m1))
-;;     (compliance-test m1)))
+(deftest sequence-test
+  (is (= '(1.0 2.0 3.0 4.0 5.0 6.0) (element-seq m)))
+  (is (= '(1.0 2.0 3.0) (element-seq (construct-matrix mpc [[1] [2] [3]]))))
+  (is (= '(1.0 2.0 3.0) (element-seq (construct-matrix mpc [[1 2 3]])))))
+
+(deftest map-test
+  (is (= (construct-matrix mpc [[2] [3] [4]]) (element-map (construct-matrix mpc [[1] [2] [3]]) inc)))
+  (is (= (construct-matrix mpc [[11] [22] [33]]) (element-map (construct-matrix mpc [[1] [2] [3]]) #(* 11 %))))
+  (is (= (construct-matrix mpc [[11] [22] [33]]) (element-map (construct-matrix mpc [[1] [2] [3]]) + (construct-matrix mpc [[10] [20] [30]])))))
+
+  ;; (deftest test-2-2-sparse
+  ;;   (compliance-test mpc)))
