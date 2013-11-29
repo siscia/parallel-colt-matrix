9fd4390625d4d50354088121955b71ad5116fc29
mikera <mike.r.anderson.13@gmail.com>
2013-11-29 09:43:26 -0800

    latest Clojure and core.matrix dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.mikera/core.matrix "0.14.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [net.mikera/core.matrix "0.16.0"]
                  [net.sourceforge.parallelcolt/parallelcolt "0.10.0"]
                  [org.clojure/tools.nrepl "0.2.0"]]
   :profiles {:dev                                                               
